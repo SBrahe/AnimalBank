669748fbcbf09024c06b90fd3cadb17f619656c2
Kristian Lau Jespersen <201807157@post.au.dk>
2021-04-25 13:38:55 +0200

    pushing before swithcing to camera x api

--- app/src/main/java/dk/au/mad21spring/animalbank/CameraActivity.java
+++ app/src/main/java/dk/au/mad21spring/animalbank/CameraActivity.java
@@ -2,13 +2,38 @@ package dk.au.mad21spring.animalbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 
+import static dk.au.mad21spring.animalbank.Constants.CAMERA_ID;
+
+//Inspiration taken from https://developer.android.com/training/camera/cameradirect
+
+
+
 public class CameraActivity extends AppCompatActivity {
+
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+    }
+
+    boolean tryOpenCamera(){
+        boolean launchSuccess = false;
+        try {
+            this.camera = Camera.open(CAMERA_ID);
+            launchSuccess = (this.camera != null);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return launchSuccess;
+    }
+
+    void releaseCamera(){
+
     }
 }
---
+++ app/src/main/java/dk/au/mad21spring/animalbank/Constants.java
@@ -0,0 +1,5 @@
+package dk.au.mad21spring.animalbank;
+
+public class Constants {
+    public static final int CAMERA_ID = 999;
+}
