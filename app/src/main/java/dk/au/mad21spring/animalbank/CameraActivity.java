3a8404db3ceb7d46eabf16bf0e77f4a8f3335db8
oskarvedel <oskar.vedel@gmail.com>
2021-04-25 13:39:26 +0200

    set up structure for info view + some navigation

--- app/src/main/java/dk/au/mad21spring/animalbank/CameraActivity.java
+++ app/src/main/java/dk/au/mad21spring/animalbank/CameraActivity.java
@@ -2,13 +2,33 @@ package dk.au.mad21spring.animalbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        btnAdd = findViewById(R.id.addBtn);
+
+        //initialize add button
+        btnAdd.setOnClickListener(v -> onAddPressed());
     }
+
+    public void onAddPressed() {
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
 }
--- app/src/main/java/dk/au/mad21spring/animalbank/InfoActivity.java
+++ app/src/main/java/dk/au/mad21spring/animalbank/InfoActivity.java
@@ -2,13 +2,54 @@ package dk.au.mad21spring.animalbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
+
+    private Button btnBack;
+    private Button btnDelete;
+    private TextView txtAnimalName;
+    private TextView txtSpottedDateTitle;
+    private TextView txtSpottedDate;
+    private TextView txtSpottedNearTitle;
+    private TextView txtSpottedNear;
+    private TextView txtUserNotesTitle;
+    private TextView txtUserNotes;
+    private TextView txtWikiNotesTitle;
+    private TextView txtWikiNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        //initialize widgets
+        btnBack = findViewById(R.id.backBtn);
+        btnDelete = findViewById(R.id.deleteBtn);
+        txtAnimalName = findViewById(R.id.animalNameText);
+        txtSpottedDateTitle = findViewById(R.id.spottedDateTitleText);
+        txtSpottedDate = findViewById(R.id.spottedDateText);
+        txtSpottedNearTitle = findViewById(R.id.spottedLocationTitleText);
+        txtSpottedNear = findViewById(R.id.spottedLocationText);
+        txtUserNotesTitle = findViewById(R.id.userNotesTitleText);
+        txtUserNotes = findViewById(R.id.userNotesText);
+        txtWikiNotesTitle = findViewById(R.id.wikiNotesTitleText);
+        txtWikiNotes = findViewById(R.id.wikiNotesText);
+
+        //initialize back button
+        btnBack.setOnClickListener(v -> onBackPressed());
+    }
+
+    //go to list when back button is pressed
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        startActivity(new Intent(InfoActivity.this, ListActivity.class));
+        finish();
     }
 }
