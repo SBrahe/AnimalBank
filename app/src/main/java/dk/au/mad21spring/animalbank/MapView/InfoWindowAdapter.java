dd07cb3bfcbf08d9758cbacf18ca7cbb6f62e119
SBrahe <201807173@post.au.dk>
2021-05-10 20:03:59 +0200

    infowindow view and adapter added

---
+++ app/src/main/java/dk/au/mad21spring/animalbank/MapView/InfoWindowAdapter.java
@@ -0,0 +1,18 @@
+package dk.au.mad21spring.animalbank.MapView;
+
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class InfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+}
