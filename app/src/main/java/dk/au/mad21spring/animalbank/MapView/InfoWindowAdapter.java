7dc534d4788a537af80cf55c27910ccb528da03d
SBrahe <201807173@post.au.dk>
2021-05-10 20:39:54 +0200

    view done correctly, and adapter done

--- app/src/main/java/dk/au/mad21spring/animalbank/MapView/InfoWindowAdapter.java
+++ app/src/main/java/dk/au/mad21spring/animalbank/MapView/InfoWindowAdapter.java
@@ -1,14 +1,31 @@
 package dk.au.mad21spring.animalbank.MapView;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 
+import dk.au.mad21spring.animalbank.R;
+
 public class InfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    Context context;
+    TextView animalNameText;
+    TextView spottedDateText;
+
+    public InfoWindowAdapter(Context context){
+        this.context = context;
+    }
     @Override
     public View getInfoWindow(Marker marker) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.custom_info_window,null);
+        animalNameText = view.findViewById(R.id.animalNameText);
+        spottedDateText = view.findViewById(R.id.spottedDateText);
+        animalNameText.setText(marker.getTitle());
+        spottedDateText.setText(marker.getSnippet());
+        return view;
     }
 
     @Override
